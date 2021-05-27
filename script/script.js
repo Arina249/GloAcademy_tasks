5b1ee42a211ece982f0a42c67efd20931d151bc3
Arina Kapustinskaya <kkarina800@mail.ru>
2021-05-27 20:24:25 +0300

    Добавление всех файлов

---
+++ script/script.js
@@ -0,0 +1,14 @@
+'use strict';
+
+let money = 300;
+let income = 1000;
+let addExpenses = 90;
+let deposit = 500;
+const mission = 'increase income';
+const period = '2 years';
+
+
+alert('Hello everyone!');
+console.log('Lorem ipsum dolor sit amet');
+
+
