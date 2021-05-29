ee2c2c0071ece74a0e103dea2cd1f883047b833b
Arina Kapustinskaya <kkarina800@mail.ru>
2021-05-29 10:06:38 +0300

    Выполнено задание 2 урока

--- script/script.js
+++ script/script.js
@@ -1,14 +1,24 @@
 'use strict';
 
-let money = 300;
-let income = 1000;
-let addExpenses = 90;
-let deposit = 500;
-const mission = 'increase income';
-const period = '2 years';
+let money = 2000;
+let income = 'Фриланс';
+let addExpenses = 'Коммуналка,Такси,Интернет,Продукты,Одежда';
+let arr = addExpenses.split(',');
+let deposit = true;
+const mission = 800;
+const period = 5;
+let budgetDay = money/30;
 
+console.log('Тип данных money: ' + typeof money);
+console.log('Тип данных income: ' + typeof income);
+console.log('Тип данных deposit: ' + typeof deposit);
+console.log('Длина строки addExpenses ' + addExpenses.length);
+console.log('Период равен ' +period+' месяцев.' + ' Цель заработать ' + mission + ' долларов');
+console.log('Строка приведенная к нижнему регистру :' + addExpenses.toLowerCase());
 
-alert('Hello everyone!');
-console.log('Lorem ipsum dolor sit amet');
+for (let i = 0; i < arr.length; i++){
+    arr[i] = arr[i].toLowerCase();
+}
+console.log('Строка приведенная к нижнему регистру и разбитая на массив: ' + arr);
 
-
+console.log('Дневной бюджет равен: ' + budgetDay);
