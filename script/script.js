53ee20bd497d3d82a0367d42ef0d856aca88a31e
Arina Kapustinskaya <kkarina800@mail.ru>
2021-06-01 00:41:26 +0300

    Add task from lesson 3

--- script/script.js
+++ script/script.js
@@ -1,14 +1,66 @@
 'use strict';
 
-let money = 300;
-let income = 1000;
-let addExpenses = 90;
-let deposit = 500;
-const mission = 'increase income';
-const period = '2 years';
+let money;
+let income = 'Фриланс';
+let addExpenses = 'Коммуналка,Такси,Интернет,Продукты,Одежда';
+let arr = addExpenses.split(',');
+let deposit = true;
+const mission = 20000;
+let period;
+let budgetDay = money/30;
+
+//console.log('Тип данных money: ' + typeof money);
+//console.log('Тип данных income: ' + typeof income);
+//console.log('Тип данных deposit: ' + typeof deposit);
+//console.log('Длина строки addExpenses ' + addExpenses.length);
+//console.log('Период равен ' +period+' месяцев.' + ' Цель заработать ' + mission + ' долларов');
+//console.log('Строка приведенная к нижнему регистру :' + addExpenses.toLowerCase());
+
+for (let i = 0; i < arr.length; i++){
+    arr[i] = arr[i].toLowerCase();
+}
+//console.log('Строка приведенная к нижнему регистру и разбитая на массив: ' + arr);
+//console.log('Дневной бюджет равен: ' + budgetDay);
+
+/////////////////////////////////
+
+money = +prompt('Ваш месячный доход?');
+console.log(typeof money);
+
+addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
+console.log(typeof addExpenses);
+
+deposit = !!confirm('Есть ли у Вас депозит в банке?');
+console.log(typeof deposit);
 
 
-alert('Hello everyone!');
-console.log('Lorem ipsum dolor sit amet');
+
+let expenses1 = prompt('Введите обязательную статью расходов?'),
+    amount1 = +prompt('Во сколько это обойдется?'),
+    expenses2 = prompt('Введите обязательную статью расходов?'),
+    amount2 = +prompt('Во сколько это обойдется?');
+
+let budgetMonth = money - (amount1 + amount2);
+console.log('Бюджет на месяц ' + budgetMonth);
+
+period = (Math.ceil(mission/budgetMonth));
+console.log('Цель будет достигнута за ' + period + ' месяцев');
+
+budgetDay =(Math.floor(budgetMonth/30));
+console.log('Бюджет на день:' + budgetDay);
+
+if(budgetDay >= 1200){
+    console.log('У вас высокий уровень дохода');
+}else if(budgetDay>=600 && budgetDay<1200){
+    console.log('У вас средний уровень дохода');
+}else if(budgetDay<600 && budgetDay>=0){
+    console.log('К сожалению у вас уровень дохода ниже среднего');
+}else if (budgetDay < 0 ){
+    console.log('Что то пошло не так');
+}
+
+
+
+
 
 
